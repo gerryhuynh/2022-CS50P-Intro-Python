7164f2c6215054bbb0b372224d67c2fd2bb0b3ca
Gerry Huynh <gerry.huynh@gmail.com>
2022-04-25 21:01:39 -0400

    Create hello.py to greet user

---
+++ l0/lec0/hello.py
@@ -0,0 +1,9 @@
+# Ask user for their name
+name = input("What's your name? ")
+
+# Say hello to user
+# print("hello, " + name)
+# print("hello,", name)
+# print("hello, ", end="")
+# print(name)
+print(f"hello, {name}")
