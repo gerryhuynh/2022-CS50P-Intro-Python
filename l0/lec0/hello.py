4c5697eb8a869756a74ad3c2d32507fa8672bcfd
Gerry Huynh <gerry.huynh@gmail.com>
2022-04-26 21:08:11 -0400

    Complete hello.py with formatted  user input

--- l0/lec0/hello.py
+++ l0/lec0/hello.py
@@ -1,5 +1,18 @@
 # Ask user for their name
-name = input("What's your name? ")
+# name = input("What's your name? ")
+
+# Remove whitespace from str
+# name = name.strip()
+
+# # Capitalize user's name
+# # name = name.capitalize()
+# name = name.title()
+
+# Remove whitespace from str and capitalize user's name
+# name = name.strip().title()
+
+# Ask user for their name
+name = input("What's your name? ").strip().title()
 
 # Say hello to user
 # print("hello, " + name)
