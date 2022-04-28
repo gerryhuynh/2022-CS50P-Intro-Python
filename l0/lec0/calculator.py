08a6c09f6865ebcc8177d64903ead66a7a8a3a73
Gerry Huynh <gerry.huynh@gmail.com>
2022-04-27 20:30:06 -0400

    Update calculator.py with Functions to Square a Number

--- l0/lec0/calculator.py
+++ l0/lec0/calculator.py
@@ -1,6 +1,9 @@
-x = float(input("What's x? "))
-y = float(input("What's y? "))
+def main():
+  x = int(input("What's x? "))
+  print("x squared is", square(x))
 
-z = round(x / y, 2)
 
-print(f"{z:.2f}")
+def square(n):
+  return n * n
+
+main()
