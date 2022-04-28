1baefb323e9215aca7db6163d8edfd2e9b5e8558
Gerry Huynh <gerry.huynh@gmail.com>
2022-04-27 20:20:33 -0400

    Update hello.py with User-Defined Function

--- l0/lec0/hello.py
+++ l0/lec0/hello.py
@@ -1,22 +1,6 @@
-# Ask user for their name
-# name = input("What's your name? ")
+def hello():
+  print("hello")
 
-# Remove whitespace from str
-# name = name.strip()
-
-# # Capitalize user's name
-# # name = name.capitalize()
-# name = name.title()
-
-# Remove whitespace from str and capitalize user's name
-# name = name.strip().title()
-
-# Ask user for their name
-name = input("What's your name? ").strip().title()
-
-# Say hello to user
-# print("hello, " + name)
-# print("hello,", name)
-# print("hello, ", end="")
-# print(name)
-print(f"hello, {name}")
+name = input("What's your name? ")
+hello()
+print(name)
