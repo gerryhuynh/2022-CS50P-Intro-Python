9c59ade31ee48fecdbf58edfe7d1aaac79c6d583
Gerry Huynh <gerry.huynh@gmail.com>
2022-04-30 09:56:24 -0400

    Intro return to Break a Loop

--- l2/lec2/cat.py
+++ l2/lec2/cat.py
@@ -1,8 +1,18 @@
-while True:
-  n = int(input("What's n? "))
+def main():
+  number = get_number()
+  meow(number)
 
-  if n > 0:
-    break
 
-for _ in range(n):
-  print("meow")
+def get_number():
+  while True:
+    n = int(input("What's n? "))
+    if n > 0:
+      return n
+
+
+def meow(n):
+  for _ in range(n):
+    print("meow")
+  
+
+main()
