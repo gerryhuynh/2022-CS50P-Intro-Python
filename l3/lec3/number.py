543a6a4414ba72ec3254527f928787e203c76131
Gerry Huynh <gerry.huynh@gmail.com>
2022-05-02 18:26:31 -0400

    Using Functions for Abstraction with get_int()

--- l3/lec3/number.py
+++ l3/lec3/number.py
@@ -1,8 +1,17 @@
-while True:
-  try:
-    x = int(input("What's x? "))
-    break
-  except ValueError:
-    print("x is not an integer")
+def main():
+  x = get_int()
+  print(f"x is {x}")
 
-print(f"x is {x}")
+
+def get_int():
+  while True:
+    try:
+      x = int(input("What's x? "))
+    except ValueError:
+      print("x is not an integer")
+    else:
+      break
+  return x
+
+
+main()
