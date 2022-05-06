48cff2a04d9ad5ca885948800725163c13c7ef62
Gerry Huynh <gerry.huynh@gmail.com>
2022-05-06 13:45:21 -0400

    Use Range of Characters

--- l7-regular-expressions/lec7/validate.py
+++ l7-regular-expressions/lec7/validate.py
@@ -2,7 +2,7 @@ import re
 
 email = input("What's your email? ").strip()
 
-if re.search("^[^@]+@[^@ ]+\.edu$", email):
+if re.search("^[a-zA-Z0-9_]+@[a-zA-Z0-9_]+\.edu$", email):
   print("Valid")
 else:
   print("Invalid")
