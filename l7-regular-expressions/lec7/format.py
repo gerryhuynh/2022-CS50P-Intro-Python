d9f91372427e1a4e5a873463989e903208f85246
Gerry Huynh <gerry.huynh@gmail.com>
2022-05-06 14:26:32 -0400

    Capture Presence/Absence of White Space After Comma

--- l7-regular-expressions/lec7/format.py
+++ l7-regular-expressions/lec7/format.py
@@ -1,7 +1,7 @@
 import re
 
 name = input("What's your name? ").strip()
-matches = re.search("^(.+), (.+)$", name)
+matches = re.search("^(.+), *(.+)$", name)
 if matches:
   name = matches.group(2) + " " + matches.group(1)
 print(f"hello, {name}")
