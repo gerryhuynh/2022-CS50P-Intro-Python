7e7c2356f5d8536cfacb505e052932570641da51
Gerry Huynh <gerry.huynh@gmail.com>
2022-05-06 14:22:03 -0400

    Store Individual group() Values Instead
    Start with 1 for the first group instead of 0 as compared to indexing
    because there is already something else in spot 0 for group().
    So, 1 would be for the first group, and 2 for the second group
    (Instead of 0 for first, and 1 for second)

--- l7-regular-expressions/lec7/format.py
+++ l7-regular-expressions/lec7/format.py
@@ -3,6 +3,7 @@ import re
 name = input("What's your name? ").strip()
 matches = re.search("^(.+), (.+)$", name)
 if matches:
-  last, first = matches.groups()
+  first = matches.group(1)
+  last= matches.group(2)
   name = f"{first} {last}"
 print(f"hello, {name}")
