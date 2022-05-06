cd7ec079ab56d78158b4dc580892585e231da75d
Gerry Huynh <gerry.huynh@gmail.com>
2022-05-06 10:20:28 -0400

    Start Using re Module - Very Basic

--- l7-regular-expressions/lec7/validate.py
+++ l7-regular-expressions/lec7/validate.py
@@ -1,8 +1,8 @@
+import re
+
 email = input("What's your email? ").strip()
 
-username, domain = email.split("@")
-
-if username and domain.endswith(".edu"):
+if re.search("@", email):
   print("Valid")
 else:
   print("Invalid")
