347f52cbf0b1e1e94e5333315a2b7ca062d60dd9
Gerry Huynh <gerry.huynh@gmail.com>
2022-05-06 14:22:51 -0400

    Condense Code for Reformatting with Concatenation

--- l7-regular-expressions/lec7/format.py
+++ l7-regular-expressions/lec7/format.py
@@ -3,7 +3,5 @@ import re
 name = input("What's your name? ").strip()
 matches = re.search("^(.+), (.+)$", name)
 if matches:
-  first = matches.group(1)
-  last= matches.group(2)
-  name = f"{first} {last}"
+  name = matches.group(2) + " " + matches.group(1)
 print(f"hello, {name}")
