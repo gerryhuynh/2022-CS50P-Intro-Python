21f175794454878048b47f45153e60f4461a8b13
Gerry Huynh <gerry.huynh@gmail.com>
2022-07-14 19:23:23 -0400

    Change to Passing in Named Arguments
    In this case, this is no longer positional -- i.e. the position of the
    arguments don't matter. This is because we're now manually passing in
    the values and explicitly specifying the names of these arguments. By
    specifying the names of the arguments according to the parameters of
    the function, we are able to set what each specific argument is.

--- l9-et-cetera/lec9/unpack.py
+++ l9-et-cetera/lec9/unpack.py
@@ -1,6 +1,4 @@
 def total(galleons, sickles, knuts):
     return (galleons * 17 + sickles) * 29 + knuts
 
-coins = [100, 50, 25]
-
-print(total(*coins), "Knuts")
+print(total(galleons=100, sickles=50, knuts=25), "Knuts")
