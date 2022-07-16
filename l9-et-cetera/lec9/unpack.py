3c78620306514933598e2dc3a1d88df0c393bdc9
Gerry Huynh <gerry.huynh@gmail.com>
2022-07-16 15:05:50 -0400

    Demonstrate print() Uses Variable Number of *args
    So, we've actually variable number of arguments before in print(). The
    documentation for the print() function shows:
    print(*objects, sep=' ', end='\n', file=sys.stdout, flush=False)
    - What we can infer from what we've learned from the use of asterisk
    today is that print() takes a variable number of positional arguments
    via the *objects parameter
    - Therefore, in the print() function, there is probably a loop that
    iterates over each of the (variable number of) objects, allowing the
    print() function to print each of those one at a time
    
    Based on what we know, this is the possible code for how print() was
    implemented by the authors of Python
    
    [Q&A]
    Can we pass *args or **kwargs from one function into another function?
    - Yes
    - You might want to do this if you want to "wrap" another function --
    provide some additional functionality but still pass in all of the
    supported arguments to the underlying function as well

--- l9-et-cetera/lec9/unpack.py
+++ l9-et-cetera/lec9/unpack.py
@@ -1,5 +1,3 @@
-def f(*args, **kwargs):
-    print("Named:", kwargs)
-
-
-f(galleons=100, sickles=50, knuts=25)
+def print(*objects, sep=" ", end="\n", ...):
+    for object in objects:
+        ...
