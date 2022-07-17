d916ab769b5916339b7e7a619a2d8bf377b9858f
Gerry Huynh <gerry.huynh@gmail.com>
2022-07-17 13:48:52 -0400

    Implement filter() with a Lambda Function

--- l9-et-cetera/lec9/gryffindors.py
+++ l9-et-cetera/lec9/gryffindors.py
@@ -5,10 +5,7 @@ students = [
     {"name": "Draco", "house": "Slytherin"},
 ]
 
-def is_gryffindor(s):
-    return s["house"] == "Gryffindor"
-
-gryffindors = filter(is_gryffindor, students)
+gryffindors = filter(lambda s: s["house"] == "Gryffindor", students)
 
 for gryffindor in sorted(gryffindors, key=lambda s: s["name"]):
     print(gryffindor["name"])
